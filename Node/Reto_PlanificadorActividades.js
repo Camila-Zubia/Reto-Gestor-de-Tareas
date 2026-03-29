a77a4d7b8d89ef0b7253b8782b541e636abf7013
Camila-Zubia <cami.zubia@gmail.com>
2026-03-28 19:12:09 -0700

    Create Reto_PlanificadorActividades.js

---
+++ Node/Reto_PlanificadorActividades.js
@@ -0,0 +1,27 @@
+function prepareActivities(arreglo){
+    nuevoArreglo = [];
+    arreglo.forEach(a =>{
+        if(!nuevoArreglo.includes(a)){
+            nuevoArreglo.push(a)
+        }
+    })
+    if(nuevoArreglo.length == 0){
+        return console.log("No hay actividades")
+    }else{
+        return nuevoArreglo.sort()
+    }
+    
+}
+
+const activities1 = [3, 1, 2, 3, 4, 2, 5]
+const plannedActivities1 = prepareActivities(activities1)
+console.log(plannedActivities1) // [1, 2, 3, 4, 5]
+
+const activities2 = [6, 5, 5, 5, 5]
+const plannedActivities2 = prepareActivities(activities2)
+console.log(plannedActivities2) // [5, 6]
+
+const activities3 = []
+const plannedActivities3 = prepareActivities(activities3)
+console.log(plannedActivities3) // []
+// No hay actividades, la lista queda vacía
